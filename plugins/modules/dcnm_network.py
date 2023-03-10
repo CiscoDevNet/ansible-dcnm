9d6007bd7fed3ab0eddd8e4200519a084905579d
Mike Wiebe <mwiebe@cisco.com>
2023-03-10 16:25:50 -0500

    Re-enable ansible 2.14.2

--- plugins/modules/dcnm_network.py
+++ plugins/modules/dcnm_network.py
@@ -587,7 +587,7 @@ class DcnmNetwork:
                     del want["isAttached"]
                     attach_list.append(want)
 
-        for attach in attach_list:
+        for attach in attach_list[:]:
             for ip, ser in self.ip_sn.items():
                 if ser == attach["serialNumber"]:
                     ip_addr = ip
