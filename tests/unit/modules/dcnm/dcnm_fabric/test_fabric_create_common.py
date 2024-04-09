3fc86dfeda7e0c0aa4fa4d4770869add3cb6e5cf
Allen Robel <arobel@cisco.com>
2024-04-09 08:27:08 -1000

    FabricCreateCommon():  100% unit test coverage

--- tests/unit/modules/dcnm/dcnm_fabric/test_fabric_create_common.py
+++ tests/unit/modules/dcnm/dcnm_fabric/test_fabric_create_common.py
@@ -60,4 +60,161 @@ def test_fabric_create_common_00010(fabric_create_common) -> None:
     assert instance.verb is None
     assert instance.state == "merged"
     assert instance._payloads_to_commit == []
-    assert instance._mandatory_payload_keys == {"BGP_AS", "FABRIC_NAME"}
+    assert instance._mandatory_payload_keys == {"BGP_AS", "FABRIC_NAME", "FABRIC_TYPE"}
+
+
+def test_fabric_create_common_00020(fabric_create_common) -> None:
+    """
+    Classes and Methods
+    - FabricCommon
+        - __init__()
+    - FabricCreateCommon
+        - __init__()
+    - FabricCreateCommon
+        - _verify_payload()
+
+    Summary
+    -   Verify ``ValueError`` is raised when payload is not a `dict``.
+    """
+    key = "test_fabric_create_common_00020a"
+    payload = payloads_fabric_create_common(key)
+
+    with does_not_raise():
+        instance = fabric_create_common
+        instance._build_properties()
+
+    match = r"FabricCreateCommon\._verify_payload: payload must be a dict\."
+    with pytest.raises(ValueError, match=match):
+        instance._verify_payload(payload)
+
+
+@pytest.mark.parametrize(
+    "mandatory_key",
+    [
+        "BGP_AS",
+        "FABRIC_NAME",
+        "FABRIC_TYPE",
+    ],
+)
+def test_fabric_create_common_00021(fabric_create_common, mandatory_key) -> None:
+    """
+    Classes and Methods
+    - FabricCommon
+        - __init__()
+    - FabricCreateCommon
+        - __init__()
+    - FabricCreateCommon
+        - _verify_payload()
+
+    Summary
+    -   Verify ``ValueError`` is raised when payload is missing mandatory keys.
+    """
+    key = "test_fabric_create_common_00021a"
+    payload = payloads_fabric_create_common(key)
+
+    payload.pop(mandatory_key, None)
+
+    with does_not_raise():
+        instance = fabric_create_common
+        instance._build_properties()
+
+    match = r"FabricCreateCommon\._verify_payload: "
+    match += rf"payload is missing mandatory keys: \['{mandatory_key}'\]"
+    with pytest.raises(ValueError, match=match):
+        instance._verify_payload(payload)
+
+
+def test_fabric_create_common_00030(fabric_create_common) -> None:
+    """
+    Classes and Methods
+    - FabricCommon
+        - __init__()
+    - FabricCreateCommon
+        - __init__()
+        - _set_fabric_create_endpoint
+
+    Summary
+    - ``ValueError`` is raised when payload contains invalid ``FABRIC_TYPE``.
+    """
+    key = "test_fabric_create_common_00030a"
+    payload = payloads_fabric_create_common(key)
+
+    with does_not_raise():
+        instance = fabric_create_common
+        instance._build_properties()
+
+    match = r"FabricCreateCommon\.fabric_type: FABRIC_TYPE must be one of "
+    match += r"\['VXLAN_EVPN'\]. "
+    match += "Got INVALID_FABRIC_TYPE"
+    with pytest.raises(ValueError, match=match):
+        instance._set_fabric_create_endpoint(payload)
+
+
+def test_fabric_create_common_00031(fabric_create_common) -> None:
+    """
+    Classes and Methods
+    - FabricCommon
+        - __init__()
+    - FabricCreateCommon
+        - __init__()
+        - _set_fabric_create_endpoint
+
+    Summary
+    -   ``ValueError`` is raised when FabricCommon().fabric_type_to_template_name_map
+        does not contain ``FABRIC_TYPE``.
+    -   Since test 00030 already tests for invalid ``FABRIC_TYPE``, this test simulates
+        the error condition by removing a valid key (``VXLAN_EVPN``) from
+        ``fabric_type_to_template_name_map``.
+    """
+    key = "test_fabric_create_common_00031a"
+    payload = payloads_fabric_create_common(key)
+
+    with does_not_raise():
+        instance = fabric_create_common
+        instance._build_properties()
+        instance.fabric_type_to_template_name_map.pop("VXLAN_EVPN", None)
+
+    match = r"FabricCreateCommon\.fabric_type_to_template_name: "
+    match += "Unknown fabric type: VXLAN_EVPN"
+    with pytest.raises(ValueError, match=match):
+        instance._set_fabric_create_endpoint(payload)
+
+
+def test_fabric_create_common_00032(monkeypatch, fabric_create_common) -> None:
+    """
+    Classes and Methods
+    - FabricCommon
+        - __init__()
+    - FabricCreateCommon
+        - __init__()
+        - _set_fabric_create_endpoint
+        - endpoints.fabric_create
+
+    Summary
+    -   ``ValueError`` is raised when endpoints.fabric_create() raises an exception.
+    -   Since ``fabric_name`` and ``template_name`` are already verified in
+        _set_fabric_create_endpoint, ApiEndpoints().fabric_create() needs
+        to be mocked to raise an exception.
+    """
+    key = "test_fabric_create_common_00032a"
+    payload = payloads_fabric_create_common(key)
+
+    class MockApiEndpoints:  # pylint: disable=too-few-public-methods
+        """
+        Mock the ApiEndpoints.fabric_create() method to raise an exception.
+        """
+        @property
+        def fabric_create(self):
+            """
+            Mocked method
+            """
+            raise ValueError("mocked exception")
+
+    with does_not_raise():
+        instance = fabric_create_common
+        instance.endpoints = MockApiEndpoints()
+        instance._build_properties()
+
+    match = "mocked exception"
+    with pytest.raises(ValueError, match=match):
+        instance._set_fabric_create_endpoint(payload)
