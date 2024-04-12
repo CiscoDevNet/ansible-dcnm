d153eb9923df5c979621bb947b615775a3914e4a
Allen Robel <arobel@cisco.com>
2024-04-12 10:06:01 -1000

    FabricDelete(): Add unit test for fabric does not exist case.

--- tests/unit/modules/dcnm/dcnm_fabric/test_fabric_delete.py
+++ tests/unit/modules/dcnm/dcnm_fabric/test_fabric_delete.py
@@ -618,3 +618,103 @@ def test_fabric_delete_00042(monkeypatch, fabric_delete) -> None:
     assert False not in instance.results.failed
     assert False in instance.results.changed
     assert True not in instance.results.changed
+
+
+def test_fabric_delete_00043(monkeypatch, fabric_delete) -> None:
+    """
+    Classes and Methods
+    - FabricCommon()
+        - __init__()
+        - payloads setter
+    - FabricDetails()
+        - __init__()
+        - refresh_super()
+    - FabricDetailsByName()
+        - __init__()
+        - refresh()
+    - FabricDelete
+        - __init__()
+        - commit()
+
+    Summary
+    -   Verify successful fabric delete code path (fabric does not exist).
+    -   The user attempts to delete a fabric and the fabric does not exist
+        on the controller.
+
+    Code Flow
+    -   FabricDelete.commit() calls FabricDelete()._validate_commit_parameters()
+        which succeeds since all required parameters are set.
+    -   FabricDelete.commit() calls FabricDelete()._get_fabrics_to_delete()
+    -   FabricDelete()._get_fabrics_to_delete() calls
+        FabricDetails().refresh() which returns a dict with keys
+        DATA == [], RETURN_CODE == 200
+    -   FabricDelete()._get_fabrics_to_delete() sets
+        FabricDelete()._fabrics_to_delete to an empty list.
+    -   FabricDelete().commit() sets results and calls register_result().
+    -   FabricDelete().register_result() registers the results of the fabric
+        delete operation.
+    """
+    method_name = inspect.stack()[0][3]
+    key = f"{method_name}a"
+
+    PATCH_DCNM_SEND = "ansible_collections.cisco.dcnm.plugins."
+    PATCH_DCNM_SEND += "module_utils.common.rest_send.dcnm_send"
+
+    def responses():
+        yield responses_fabric_details(key)
+        yield responses_fabric_summary(key)
+
+    gen = ResponseGenerator(responses())
+
+    def mock_dcnm_send(*args, **kwargs):
+        item = gen.next
+        return item
+
+    with does_not_raise():
+        instance = fabric_delete
+        instance.fabric_names = ["f1"]
+
+        instance.fabric_details = FabricDetailsByName(params)
+        instance.fabric_details.rest_send = RestSend(MockAnsibleModule())
+        instance.fabric_details.rest_send.unit_test = True
+
+        instance.fabric_summary = FabricSummary(params)
+        instance.fabric_summary.rest_send = RestSend(MockAnsibleModule())
+        instance.fabric_summary.rest_send.unit_test = True
+
+        instance.rest_send = RestSend(MockAnsibleModule())
+        instance.rest_send.unit_test = True
+
+        instance.results = Results()
+
+    monkeypatch.setattr(PATCH_DCNM_SEND, mock_dcnm_send)
+    with does_not_raise():
+        instance.commit()
+
+    assert isinstance(instance.results.diff, list)
+    assert isinstance(instance.results.result, list)
+    assert isinstance(instance.results.response, list)
+
+    assert len(instance.results.diff) == 1
+    assert len(instance.results.metadata) == 1
+    assert len(instance.results.response) == 1
+    assert len(instance.results.result) == 1
+
+    assert instance.results.diff[0].get("sequence_number", None) == 1
+    assert instance.results.diff[0].get("fabric_name", None) == None
+
+    assert instance.results.metadata[0].get("action", None) == "delete"
+    assert instance.results.metadata[0].get("check_mode", None) is False
+    assert instance.results.metadata[0].get("sequence_number", None) == 1
+    assert instance.results.metadata[0].get("state", None) == "deleted"
+
+    assert instance.results.response[0].get("RETURN_CODE", None) == 200
+    assert instance.results.response[0].get("MESSAGE", None)  == "No fabrics to delete"
+
+    assert instance.results.result[0].get("changed", None) is False
+    assert instance.results.result[0].get("success", None) is True
+
+    assert False in instance.results.failed
+    assert True not in instance.results.failed
+    assert False in instance.results.changed
+    assert True not in instance.results.changed
