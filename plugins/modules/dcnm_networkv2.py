367e42d360c23593c3309205adde90a585d8e519
Praveen Ramoorthy <prramoor@cisco.com>
2024-05-08 12:08:59 +0530

    fix copyright issues

--- plugins/modules/dcnm_networkv2.py
+++ plugins/modules/dcnm_networkv2.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python
 #
-# Copyright (c) 2020-2023 Cisco and/or its affiliates.
+# Copyright (c) 2024 Cisco and/or its affiliates.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
