8f788c06c6e9de2cc27352052c30767b8c1e5e05
Allen Robel <arobel@cisco.com>
2024-05-23 11:10:44 -1000

    Implement query state.
    1. Common().get_have(): Move method to Merged()
    
    2. Merged().get_have(): Added from Common() and slightly modified.
    
    3. Query().get_have(): New method - differs from Merged().get_have() in that it doesn't call fail_json() if mode is "inconsistent" or "migration".
    
    4. main(): remove commented code.

--- plugins/modules/dcnm_maintenance_mode.py
+++ plugins/modules/dcnm_maintenance_mode.py
@@ -344,7 +344,6 @@ class Common:
         self.have = {}
         self.want = []
         self.query = []
-        self._implemented_states = set()
         # populated in self._merge_global_and_switch_configs()
         self.switch_configs = []
 
@@ -352,81 +351,14 @@ class Common:
         self._properties = {}
         self._properties["ansible_module"] = None
 
-    def get_have(self):
-        """
-        Caller: main()
-
-        Build self.have, a dict containing the current mode of all switches.
-
-        Have is a dict, keyed on switch_ip, where each element is a dict
-        with the following structure:
-
-        ```json
-        {
-            "192.169.1.2": {
-                fabric_name: "MyFabric",
-                maintenance_mode: "Maintenance",
-                serial_number: "FCI1234567"
-            },
-            "192.169.1.3": {
-                fabric_name: "YourFabric",
-                maintenance_mode: "Normal",
-                serial_number: "FCH2345678"
-            }
-        }
-        ```
-        """
-        method_name = inspect.stack()[0][3]  # pylint: disable=unused-variable
-        self.switch_details.rest_send = RestSend(self.ansible_module)
-        self.switch_details.refresh()
-        self.have = {}
-        # self.config has already been validated
-        for switch in self.config.get("switches"):
-            ip_address = switch.get("ip_address")
-            self.switch_details.filter = ip_address
-            try:
-                serial_number = self.switch_details.serial_number
-            except ValueError as error:
-                self.ansible_module.fail_json(f"{error}", **self.results.failed_result)
-            if serial_number is None:
-                msg = f"{self.class_name}.{method_name}: "
-                msg += f"Switch with ip_address {ip_address} "
-                msg += "does not exist on the controller."
-                self.ansible_module.fail_json(msg, **self.results.failed_result)
-            mode = self.switch_details.maintenance_mode
-            if mode == "inconsistent":
-                msg = f"{self.class_name}.{method_name}: "
-                msg += "Switch maintenance mode state differs from the "
-                msg += "controller's maintenance mode state for switch "
-                msg += f"with ip_address {ip_address}. This is typically "
-                msg += "resolved by initiating a switch Deploy Config on "
-                msg += "the controller."
-                self.ansible_module.fail_json(msg, **self.results.failed_result)
-            if mode == "migration":
-                msg = f"{self.class_name}.{method_name}: "
-                msg += "Switch maintenance mode is in migration state for the "
-                msg += f"switch with ip_address {ip_address}. "
-                msg += "This indicates that the switch configuration is not "
-                msg += "compatible with the switch role in the hosting "
-                msg += "fabric.  The issue might be resolved by initiating a "
-                msg += "fabric Recalculate & Deploy on the controller. "
-                msg += "Failing that, the switch configuration might need to be "
-                msg += "manually modified to match the switch role in the "
-                msg += "hosting fabric."
-                self.ansible_module.fail_json(msg, **self.results.failed_result)
-            fabric_name = self.switch_details.fabric_name
-            self.have[ip_address] = {}
-            self.have[ip_address].update({"maintenance_mode": mode})
-            self.have[ip_address].update({"serial_number": serial_number})
-            self.have[ip_address].update({"fabric_name": fabric_name})
-
     def get_want(self) -> None:
         """
-        Caller: main()
+        ### Summary
+        Build self.want, a list of validated playbook configurations.
 
-        1. Merge the global config into each switch config
-        2. Validate the merged configs
-        3. Populate self.want with the validated configs
+        1. Merge the playbook global config into each switch config.
+        2. Validate the merged configs from step 1 against the param spec.
+        3. Populate self.want with the validated configs.
 
         ### self.want structure
 
@@ -439,8 +371,8 @@ class Common:
             },
             {
                 "ip_address": "192.168.1.3",
-                "mode": "maintenance",
-                "deploy": false
+                "mode": "normal",
+                "deploy": true
             }
         ]
         ```
@@ -480,14 +412,15 @@ class Common:
 
     def _merge_global_and_switch_configs(self, config) -> None:
         """
-        Merge the global config with each switch config and
-        populate list of merged configs self.switch_configs.
+        ### Summary
+        Merge the global playbook config with each switch config and
+        populate a list of merged configs (``self.switch_configs``).
 
-        Merge rules:
-        1.  switch_config takes precedence over global_config.
-        2.  If switch_config is missing a parameter, use parameter
-            from global_config.
-        3.  If switch_config has a parameter, use it.
+        ### Merge rules
+            -   switch_config takes precedence over global_config.
+            -   If switch_config is missing a parameter, use parameter
+                from global_config.
+            -   If switch_config has a parameter, use it.
         """
         method_name = inspect.stack()[0][3]
 
@@ -561,7 +494,87 @@ class Merged(Common):
 
         self.need = []
 
-        self._implemented_states.add("merged")
+    def get_have(self):
+        """
+        ### Summary
+        Build self.have, a dict containing the current mode of all switches.
+
+        Have is a dict, keyed on switch_ip, where each element is a dict
+        with the following structure:
+        -   ``fabric_name``: The name of the switch's hosting fabric.
+        -   ``mode``: The current maintenance mode of the switch.
+        -   ``role``: The role of the switch in the hosting fabric.
+        -   ``serial_number``: The serial number of the switch.
+
+        ```json
+        {
+            "192.169.1.2": {
+                fabric_name: "MyFabric",
+                mode: "maintenance",
+                role: "spine",
+                serial_number: "FCI1234567"
+            },
+            "192.169.1.3": {
+                fabric_name: "YourFabric",
+                mode: "normal",
+                role: "leaf",
+                serial_number: "FCH2345678"
+            }
+        }
+        ```
+        ### NOTES
+        -   We are not currently using ``role``.  We added it to improve
+            error messages, but will need to pass this to MaintenanceMode()
+            in order to do so.  This will require adding a ``role`` property
+            to MaintenanceMode().  But ``role`` is not strictly needed for the
+            MaintenanceMode() class.  Hence, we're not adding this now.  Maybe
+            in a future release.
+        """
+        method_name = inspect.stack()[0][3]  # pylint: disable=unused-variable
+        self.switch_details.rest_send = RestSend(self.ansible_module)
+        self.switch_details.refresh()
+        self.have = {}
+        # self.config has already been validated
+        for switch in self.config.get("switches"):
+            ip_address = switch.get("ip_address")
+            self.switch_details.filter = ip_address
+            try:
+                serial_number = self.switch_details.serial_number
+            except ValueError as error:
+                self.ansible_module.fail_json(f"{error}", **self.results.failed_result)
+            if serial_number is None:
+                msg = f"{self.class_name}.{method_name}: "
+                msg += f"Switch with ip_address {ip_address} "
+                msg += "does not exist on the controller."
+                self.ansible_module.fail_json(msg, **self.results.failed_result)
+            mode = self.switch_details.maintenance_mode
+            if mode == "inconsistent":
+                msg = f"{self.class_name}.{method_name}: "
+                msg += "Switch maintenance mode state differs from the "
+                msg += "controller's maintenance mode state for switch "
+                msg += f"with ip_address {ip_address}. This is typically "
+                msg += "resolved by initiating a switch Deploy Config on "
+                msg += "the controller."
+                self.ansible_module.fail_json(msg, **self.results.failed_result)
+            if mode == "migration":
+                msg = f"{self.class_name}.{method_name}: "
+                msg += "Switch maintenance mode is in migration state for the "
+                msg += f"switch with ip_address {ip_address}. "
+                msg += "This indicates that the switch configuration is not "
+                msg += "compatible with the switch role in the hosting "
+                msg += "fabric.  The issue might be resolved by initiating a "
+                msg += "fabric Recalculate & Deploy on the controller. "
+                msg += "Failing that, the switch configuration might need to be "
+                msg += "manually modified to match the switch role in the "
+                msg += "hosting fabric."
+                self.ansible_module.fail_json(msg, **self.results.failed_result)
+            fabric_name = self.switch_details.fabric_name
+            role = self.switch_details.role
+            self.have[ip_address] = {}
+            self.have[ip_address].update({"fabric_name": fabric_name})
+            self.have[ip_address].update({"mode": mode})
+            self.have[ip_address].update({"role": role})
+            self.have[ip_address].update({"serial_number": serial_number})
 
     def get_need(self):
         """
@@ -576,30 +589,30 @@ class Merged(Common):
             "172.22.150.2": {
                 "deploy": false
                 "fabric_name": "MyFabric",
-                "maintenance_mode": "maintenance",
+                "mode": "maintenance",
                 "serial_number": "FCI1234567"
             },
             "172.22.150.3": {
                 "deploy": true
                 "fabric_name": "YourFabric",
-                "maintenance_mode": "normal",
+                "mode": "normal",
                 "serial_number": "HMD2345678"
             }
         }
         """
         self.need = {}
         for want in self.want:
-            want_ip = want.get("ip_address", None)
-            if want_ip not in self.have:
+            key = want.get("ip_address", None)
+            if key not in self.have:
                 continue
-            serial_number = self.have[want_ip]["serial_number"]
-            fabric_name = self.have[want_ip]["fabric_name"]
-            if want.get("mode") != self.have[want_ip]["maintenance_mode"]:
-                self.need[want_ip] = want
-                self.need[want_ip].update({"deploy": want.get("deploy")})
-                self.need[want_ip].update({"fabric_name": fabric_name})
-                self.need[want_ip].update({"serial_number": serial_number})
-                self.need[want_ip].update({"maintenance_mode": want.get("mode")})
+            serial_number = self.have[key]["serial_number"]
+            fabric_name = self.have[key]["fabric_name"]
+            if want.get("mode") != self.have[key]["mode"]:
+                self.need[key] = want
+                self.need[key].update({"deploy": want.get("deploy")})
+                self.need[key].update({"fabric_name": fabric_name})
+                self.need[key].update({"serial_number": serial_number})
+                self.need[key].update({"mode": want.get("mode")})
 
     def commit(self):
         """
@@ -637,7 +650,7 @@ class Merged(Common):
         instance.rest_send = RestSend(self.ansible_module)
         instance.results = self.results
         for ip_address, switch in self.need.items():
-            mode = switch.get("maintenance_mode", None)
+            mode = switch.get("mode", None)
             serial_number = switch.get("serial_number", None)
             fabric_name = switch.get("fabric_name", None)
             deploy = switch.get("deploy", False)
@@ -661,7 +674,6 @@ class Query(Common):
     def __init__(self, ansible_module):
         self.class_name = self.__class__.__name__
         super().__init__(ansible_module)
-        method_name = inspect.stack()[0][3]  # pylint: disable=unused-variable
 
         self.log = logging.getLogger(f"dcnm.{self.class_name}")
 
@@ -670,41 +682,81 @@ class Query(Common):
         msg += f"check_mode: {self.check_mode}"
         self.log.debug(msg)
 
-        self._implemented_states.add("query")
-
-    def commit(self) -> None:
+    def get_have(self):
         """
-        1.  query the switches in self.want that exist on the controller
+        ### Summary
+        Build self.have, a dict containing the current mode of all switches.
+
+        Have is a dict, keyed on switch_ip, where each element is a dict
+        with the following structure:
+        -   ``fabric_name``: The name of the switch's hosting fabric.
+        -   ``mode``: The current maintenance mode of the switch.
+        -   ``role``: The role of the switch in the hosting fabric.
+        -   ``serial_number``: The serial number of the switch.
+
+        ```json
+        {
+            "192.169.1.2": {
+                fabric_name: "MyFabric",
+                mode: "maintenance",
+                role: "spine",
+                serial_number: "FCI1234567"
+            },
+            "192.169.1.3": {
+                fabric_name: "YourFabric",
+                mode: "normal",
+                role: "leaf",
+                serial_number: "FCH2345678"
+            }
+        }
+        ```
         """
         method_name = inspect.stack()[0][3]  # pylint: disable=unused-variable
-
-        self.get_want()
-
         self.switch_details.rest_send = RestSend(self.ansible_module)
         self.switch_details.refresh()
+        self.have = {}
         # self.config has already been validated
-        for item in self.want:
-            ip_address = item.get("ip_address")
+        for switch in self.config.get("switches"):
+            ip_address = switch.get("ip_address")
             self.switch_details.filter = ip_address
-            serial_number = self.switch_details.serial_number
+            try:
+                serial_number = self.switch_details.serial_number
+            except ValueError as error:
+                self.ansible_module.fail_json(f"{error}", **self.results.failed_result)
             if serial_number is None:
                 msg = f"{self.class_name}.{method_name}: "
                 msg += f"Switch with ip_address {ip_address} "
                 msg += "does not exist on the controller."
                 self.ansible_module.fail_json(msg, **self.results.failed_result)
-            mode = self.switch_details.mode
+
+            mode = self.switch_details.maintenance_mode
+            role = self.switch_details.switch_role
             fabric_name = self.switch_details.fabric_name
-            self.results.diff_current = {
-                "fabric_name": fabric_name,
-                "ip_address": ip_address,
-                "mode": mode,
-                "serial_number": serial_number,
-            }
-            self.results.changed = False
-            self.results.action = "query"
-            self.results.failed = False
-            self.results.result_current = {"changed": False, "success": True}
-            self.results.register_task_result()
+            self.have[ip_address] = {}
+            self.have[ip_address].update({"mode": mode})
+            if role is not None:
+                self.have[ip_address].update({"role": role})
+            else:
+                self.have[ip_address].update({"role": "na"})
+            self.have[ip_address].update({"serial_number": serial_number})
+            self.have[ip_address].update({"fabric_name": fabric_name})
+
+    def commit(self) -> None:
+        """
+        ### Summary
+        Query the switches in self.want that exist on the controller
+        and update ``self.results`` with the query results.
+        """
+        self.get_want()
+        self.get_have()
+
+        # If we got this far, the request was successful.
+        self.results.diff_current = self.have
+        self.results.changed = False
+        self.results.action = "query"
+        self.results.failed = False
+        self.results.result_current = {"changed": False, "success": True}
+        self.results.register_task_result()
 
 
 def main():
@@ -715,17 +767,6 @@ def main():
         "required": True,
         "type": "dict",
     }
-    # argument_spec["deploy"] = {
-    #     "default": True,
-    #     "required": False,
-    #     "type": "bool",
-    # }
-    # argument_spec["mode"] = {
-    #     "choices": ["Maintenance", "Normal"],
-    #     "default": "Maintenance",
-    #     "required": False,
-    #     "type": "str"
-    # }
     argument_spec["state"] = {
         "choices": ["merged", "query"],
         "default": "merged",
