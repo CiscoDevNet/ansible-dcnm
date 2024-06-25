19e9b1ffee1db82f8f63b3e100da18f69b8cf1f2
Allen Robel <arobel@cisco.com>
2024-06-24 14:54:11 -1000

    Replaced(): Create fabrics if they do not exist.
    Replaced(): If fabrics in the playbook config for replaced state do not exist, instantiate and configure Merged() in Replaced().send_need_replaced(), and call Merged().send_need_create().

--- plugins/modules/dcnm_fabric.py
+++ plugins/modules/dcnm_fabric.py
@@ -2404,16 +2404,17 @@ class Replaced(Common):
         self.fabric_replaced = FabricReplacedBulk(self.params)
         self.fabric_summary = FabricSummary(self.params)
         self.fabric_types = FabricTypes()
+        self.merged = None
+        self.need_create = []
+        self.need_replaced = []
         self.template = TemplateGet()
+        self._implemented_states.add("replaced")
 
         msg = f"ENTERED Replaced.{method_name}: "
         msg += f"state: {self.state}, "
         msg += f"check_mode: {self.check_mode}"
         self.log.debug(msg)
 
-        self.need_replaced = []
-
-        self._implemented_states.add("replaced")
 
     def get_need(self):
         """
@@ -2423,8 +2424,11 @@ class Replaced(Common):
         """
         self.payloads = {}
         for want in self.want:
-            # Skip fabrics that do not exist on the controller
+            # If fabrics do not exist on the controller, add them to
+            # need_create.  These will be created by Merged() in
+            # Replaced.send_need_replaced()
             if want["FABRIC_NAME"] not in self.have.all_data:
+                self.need_create.append(want)
                 continue
             self.need_replaced.append(want)
 
@@ -2458,6 +2462,17 @@ class Replaced(Common):
         msg += "self.need_replaced: "
         msg += f"{json_pretty(self.need_replaced)}"
         self.log.debug(msg)
+
+        if len(self.need_create) != 0:
+            self.merged = Merged(self.params)
+            self.merged.ansible_module = self.ansible_module
+            self.merged.ansible_module = self.ansible_module
+            self.merged.rest_send = self.rest_send
+            self.merged.fabric_details.rest_send = self.rest_send
+            self.merged.fabric_summary.rest_send = self.rest_send
+            self.merged.results = self.results
+            self.merged.need_create = self.need_create
+            self.merged.send_need_create()
 
         if len(self.need_replaced) == 0:
             msg = f"{self.class_name}.{method_name}: "
