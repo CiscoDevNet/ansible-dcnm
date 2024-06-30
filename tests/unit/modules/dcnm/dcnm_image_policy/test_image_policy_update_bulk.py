fa3ded1ef4d6d6b0a1ab3ca0856cf5a0b6305296
Allen Robel <arobel@cisco.com>
2024-06-30 10:25:48 -1000

    test_image_policy_update_bulk.py: Remove unused import.
    Remove GenerateResponses() which is superceded by ResponseGenerator()

--- tests/unit/modules/dcnm/dcnm_image_policy/test_image_policy_update_bulk.py
+++ tests/unit/modules/dcnm/dcnm_image_policy/test_image_policy_update_bulk.py
@@ -44,9 +44,8 @@ from ansible_collections.cisco.dcnm.plugins.module_utils.common.sender_file impo
 from ansible_collections.cisco.dcnm.tests.unit.module_utils.common.common_utils import \
     ResponseGenerator
 from ansible_collections.cisco.dcnm.tests.unit.modules.dcnm.dcnm_image_policy.utils import (
-    GenerateResponses, MockAnsibleModule, does_not_raise,
-    image_policy_update_bulk_fixture, params,
-    payloads_image_policy_update_bulk, responses_ep_policies,
+    MockAnsibleModule, does_not_raise, image_policy_update_bulk_fixture,
+    params, payloads_image_policy_update_bulk, responses_ep_policies,
     responses_ep_policy_edit, responses_image_policy_update_bulk,
     rest_send_result_current)
 
