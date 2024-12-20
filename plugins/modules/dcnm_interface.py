f77ab268457fcb59c7b2126e6c7b9fa350c1d7e8
Charly Coueffe <75327499+ccoueffe@users.noreply.github.com>
2024-12-20 18:32:54 +0100

    Update dcnm_interface.py (#359)
    Update if condition for sub_int to support two regex.
    * EthernetX/yyy.zzz
    * PortX.zzzzz

--- plugins/modules/dcnm_interface.py
+++ plugins/modules/dcnm_interface.py
@@ -1887,8 +1887,12 @@ class DcnmIntf:
             port_id = re.findall(r"\d+", name)
             return ("vPC" + str(port_id[0]), port_id[0])
         if "sub_int" == if_type:
-            port_id = re.findall(r"\d+\/\d+.\d+", name)
-            return ("Ethernet" + str(port_id[0]), port_id[0])
+            if re.findall(r"\d+\/\d+.\d+", name):
+                port_id = re.findall(r"\d+\/\d+.\d+", name)
+                return ("Ethernet" + str(port_id[0]), port_id[0])
+            if re.findall(r"\d+.\d+", name):
+                port_id = re.findall(r"\d+\.\d+", name)
+                return ("Port-channel" + str(port_id[0]), port_id[0])
         if "lo" == if_type:
             port_id = re.findall(r"\d+", name)
             return ("Loopback" + str(port_id[0]), port_id[0])
