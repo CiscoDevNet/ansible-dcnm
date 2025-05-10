9ff50a915696d694a58ce916bbae7e357231c098
Allen Robel <arobel@cisco.com>
2025-05-10 22:31:05 +0200

    Forgot to add model in last commit
    Oops.  Forgot to add the new model.  Adding now…

---
+++ plugins/module_utils/vrf/controller_response_generic_v12.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+from typing import Any, Optional, Union
+
+from pydantic import BaseModel, ConfigDict, Field
+
+class ControllerResponseGenericV12(BaseModel):
+    """
+    # Summary
+
+    Generic response model for the controller.
+
+    ## Raises
+
+    ValueError if validation fails
+    """
+    model_config = ConfigDict(
+        str_strip_whitespace=True,
+        validate_assignment=True,
+    )
+
+    DATA: Optional[Union[list, dict, str]] = Field(default="")
+    ERROR: Optional[str] = Field(default="")
+    MESSAGE: Optional[str] = Field(default="")
+    METHOD: Optional[str] = Field(default="")
+    RETURN_CODE: Optional[int] = Field(default=500)
