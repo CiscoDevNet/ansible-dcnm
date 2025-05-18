9349ba9501d94ed7f440006c759e2ff9c2ba2c6e
Allen Robel <arobel@cisco.com>
2025-05-18 13:11:56 -1000

    Align VrfPayloadV12 with other models
    1. plugins/module_utils/vrf_controller_payload_v12.py
    
    - Align with other models by adding Field(alias=“<controllerParamName>”) to all parameters and changing the parameter name to snake case.
    
    - Add a field validator for serviceVrfTemplate to convert it from None to “” prior to model validation.

--- plugins/module_utils/vrf/vrf_controller_payload_v12.py
+++ plugins/module_utils/vrf/vrf_controller_payload_v12.py
@@ -6,9 +6,10 @@ Path: /appcenter/cisco/ndfc/api/v1/lan-fabric/rest/top-down/fabrics/{fabric_name
 Verb: POST
 """
 
+from typing import Union
 import warnings
 
-from pydantic import BaseModel, ConfigDict, Field, PydanticExperimentalWarning, model_validator
+from pydantic import BaseModel, ConfigDict, Field, PydanticExperimentalWarning, field_validator, model_validator
 from typing_extensions import Self
 
 from .vrf_template_config_v12 import VrfTemplateConfigV12
@@ -97,20 +98,31 @@ class VrfPayloadV12(BaseModel):
 
     model_config = base_vrf_model_config
 
-    fabric: str = Field(..., max_length=64, description="Fabric name in which the VRF resides.")
-    hierarchicalKey: str = Field(default="", max_length=64)
-    serviceVrfTemplate: str = Field(default="")
-    tenantName: str = Field(default="")
-    vrfId: int = Field(..., ge=1, le=16777214)
-    vrfName: str = Field(..., min_length=1, max_length=32, description="Name of the VRF, 1-32 characters.")
-    vrfTemplate: str = Field(default="Default_VRF_Universal")
-    vrfTemplateConfig: VrfTemplateConfigV12
+    fabric: str = Field(..., alias="fabric", max_length=64, description="Fabric name in which the VRF resides.")
+    hierarchical_key: str = Field(alias="hierarchicalKey", default="", max_length=64)
+    service_vrf_template: str = Field(alias="serviceVrfTemplate", default="")
+    tenant_name: str = Field(alias="tenantName", default="")
+    vrf_id: int = Field(..., alias="vrfId", ge=1, le=16777214)
+    vrf_name: str = Field(..., alias="vrfName", min_length=1, max_length=32, description="Name of the VRF, 1-32 characters.")
+    vrf_template: str = Field(alias="vrfTemplate", default="Default_VRF_Universal")
+    vrf_template_config: VrfTemplateConfigV12 = Field(alias="vrfTemplateConfig")
+
+    @field_validator("service_vrf_template", mode="before")
+    @classmethod
+    def validate_service_vrf_template(cls, value: Union[str, None]) -> str:
+        """
+        Validate serviceVrfTemplate.  If it is not empty, it must be a valid
+        service VRF template.
+        """
+        if value is None:
+            return ""
+        return value
 
     @model_validator(mode="after")
     def validate_hierarchical_key(self) -> Self:
         """
         If hierarchicalKey is "", set it to the fabric name.
         """
-        if self.hierarchicalKey == "":
-            self.hierarchicalKey = self.fabric
+        if self.hierarchical_key == "":
+            self.hierarchical_key = self.fabric
         return self
