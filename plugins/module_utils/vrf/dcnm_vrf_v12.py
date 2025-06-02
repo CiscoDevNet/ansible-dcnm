c195903a095167f145528cc124de5a9ddb13a232
Allen Robel <arobel@cisco.com>
2025-06-01 15:17:59 -1000

    Update copyright

--- plugins/module_utils/vrf/dcnm_vrf_v12.py
+++ plugins/module_utils/vrf/dcnm_vrf_v12.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 # mypy: disable-error-code="import-untyped"
 #
-# Copyright (c) 2020-2023 Cisco and/or its affiliates.
+# Copyright (c) 2020-2025 Cisco and/or its affiliates.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
