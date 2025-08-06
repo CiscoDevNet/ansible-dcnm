f06d9080eb94950b9b90973b31efa03f50a6e06e
= <=>
2025-08-06 20:13:11 +0530

    Add support for body-data encoding type 'x-www-form-urlencoded'

--- plugins/modules/dcnm_rest.py
+++ plugins/modules/dcnm_rest.py
@@ -44,6 +44,11 @@ options:
     - json_data
     required: no
     type: raw
+  urlencoded_data:
+    description:
+    - 'Dictionary data to be url-encoded for x-www-form-urlencoded type REST API call'
+    required: no
+    type: raw
 author:
     - Mike Wiebe (@mikewiebe)
 """
@@ -65,6 +70,12 @@ EXAMPLES = """
     method: POST
     path: /rest/top-down/fabrics/fabric1/vrfs/attachments
     json_data: '[{"vrfName":"sales66_vrf1","lanAttachList":[{"fabric":"fabric1","vrfName":"sales66_vrf1","serialNumber":"FDO21392QKM","vlan":2000,"freeformConfig":"","deployment":false,"extensionValues":"","instanceValues":"{\"loopbackId\":\"\",\"loopbackIpAddress\":\"\",\"loopbackIpV6Address\":\"\"}"}]}]'
+
+- name: Save Robot Credentials - (urlencoded)
+  dcnm_rest:
+    method: POST
+    path: /rest/lanConfig/saveRobotCredentials
+    urlencoded_data: '{"password": "password", "username": "admin"}'
 
 # Read payload data from file and validate a template
 - set_fact:
@@ -89,6 +100,7 @@ response:
 """
 
 import json
+import urllib.parse
 from ansible.module_utils.basic import AnsibleModule
 from ansible_collections.cisco.dcnm.plugins.module_utils.network.dcnm.dcnm import (
     dcnm_send,
@@ -101,6 +113,7 @@ def main():
         method=dict(required=True, choices=["GET", "POST", "PUT", "DELETE"]),
         path=dict(required=True, type="str"),
         data=dict(type="raw", required=False, default=None, aliases=["json_data"]),
+        urlencoded_data = dict(type="raw", required=False, default=None),
     )
 
     # seed the result dict
@@ -110,17 +123,28 @@ def main():
 
     method = module.params["method"]
     path = module.params["path"]
-    for key in ["json_data", "data"]:
+    is_urlencoded = False
+
+    for key in ["json_data", "data", "urlencoded_data"]:
         data = module.params.get(key)
         if data is not None:
+            if key == "urlencoded_data":
+                is_urlencoded = True
             break
     if data is None:
         data = "{}"
 
     # Determine if this is valid JSON or not
     try:
-        json.loads(data)
-        result["response"] = dcnm_send(module, method, path, data)
+        json_data = json.loads(data)
+        if is_urlencoded:
+            # If the data is valid JSON but marked as urlencoded, we need to convert it
+            # to a URL-encoded string before sending it.
+            urlencoded_data = urllib.parse.urlencode(json_data)
+            result["response"] = dcnm_send(module, method, path, urlencoded_data, "urlencoded")
+        else:
+            # If the data is valid JSON, send it as a JSON string
+            result["response"] = dcnm_send(module, method, path, data)
     except json.JSONDecodeError:
         # Resend data as text since it's not valid JSON
         result["response"] = dcnm_send(module, method, path, data, "text")
