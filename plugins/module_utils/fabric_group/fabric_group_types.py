7b662811600b47c491c7e40221f66422ad8d19a6
Allen Robel <arobel@cisco.com>
2025-10-27 09:33:18 -1000

    FabricGroupTypes: type hints, docstring cleanup
    No functional changes in this commit.
    
    1. Remove _init_properties and move into __init__
    
    2. Add type hints to vars and method signatures
    
    3. Clean up class and method docstrings

--- plugins/module_utils/fabric_group/fabric_group_types.py
+++ plugins/module_utils/fabric_group/fabric_group_types.py
@@ -22,22 +22,36 @@ __metaclass__ = type  # pylint: disable=invalid-name
 __author__ = "Allen Robel"
 
 import copy
+import inspect
 import logging
 
 
 class FabricGroupTypes:
     """
+    # Summary
+
     Fabric group type definitions for the dcnm_fabric_group module.
 
-    Usage
+    Currently supported fabric group types:
 
+    -   MCFG - Multi-Cluster Fabric Group
+
+    ## Raises
+
+    -   `ValueError` if:
+        -   `fabric_group_type` is set to an invalid fabric group type.
+        -   `template_name`, `mandatory_parameters`, or `feature_name` is accessed before `fabric_group_type` is set.
+
+    ## Usage
+
+    ```python
     # import and instantiate the class
     from ansible_collections.cisco.dcnm.plugins.module_utils.fabric_groups.fabric_group_types import FabricGroupTypes
     fabric_group_types = FabricGroupTypes()
 
     # Access the set of valid fabric types
     valid_fabric_group_types = fabric_group_types.valid_fabric_group_types
-    <do something with valid_fabric_group_types omitted>
+    # Do something with valid_fabric_group_types omitted
 
     # Set the fabric group type for which further operations will be performed
     try:
@@ -50,42 +64,59 @@ class FabricGroupTypes:
 
     # Access mandatory parameters for the MCFG fabric group type
     mandatory_parameters = fabric_group_types.mandatory_parameters
+
+    # Access the feature name required for the MCFG fabric group type
+    feature_name = fabric_group_types.feature_name
+
+    # Access the internal fabric type name for the MCFG fabric group type
+    fabric_type = fabric_group_types.fabric_type
+    ```
+
     """
 
-    def __init__(self):
-        self.class_name = self.__class__.__name__
+    def __init__(self) -> None:
+        self.class_name: str = self.__class__.__name__
 
-        self.log = logging.getLogger(f"dcnm.{self.class_name}")
-
-        msg = f"ENTERED {self.class_name}(): "
-        self.log.debug(msg)
-
+        self.log: logging.Logger = logging.getLogger(f"dcnm.{self.class_name}")
+        self._template_name: str = ""
+        self._fabric_group_type: str = ""
         self._init_fabric_group_types()
-        self._init_properties()
 
     def _init_fabric_group_types(self) -> None:
         """
+        # Summary
+
         This is the single place to add new fabric group types.
 
-        Initialize the following:
+        Initializes the following:
+
         -   fabric_group_type_to_template_name_map dict()
         -   fabric_group_type_to_feature_name_map dict()
+        -   fabric_group_type_to_fabric_type_map dict()
         -   _valid_fabric_group_types - Sorted list() of fabric group types
         -  _mandatory_parameters_all_fabric_groups list()
         -  _mandatory_parameters dict() keyed on fabric group type
             - Value is a list of mandatory parameters for the fabric group type
+
+        ## Raises
+
+        None
         """
-        self._fabric_group_type_to_template_name_map = {}
+        self._fabric_group_type_to_template_name_map: dict[str, str] = {}
         self._fabric_group_type_to_template_name_map["MCFG"] = "MSD_Fabric"
 
         # Map fabric group type to the feature name that must be running
         # on the controller to enable the fabric group type.
-        self._fabric_group_type_to_feature_name_map = {}
+        self._fabric_group_type_to_feature_name_map: dict[str, str] = {}
         self._fabric_group_type_to_feature_name_map["MCFG"] = "vxlan"
 
-        self._valid_fabric_group_types = self._fabric_group_type_to_template_name_map.keys()
+        # Map the fabric group type to its internal FABRIC_TYPE name
+        self._fabric_group_type_to_fabric_type_map: dict[str, str] = {}
+        self._fabric_group_type_to_fabric_type_map["MCFG"] = "MFD"
 
-        self._mandatory_parameters_all_fabric_groups = []
+        self._valid_fabric_group_types: set[str] = set(self._fabric_group_type_to_template_name_map.keys())
+
+        self._mandatory_parameters_all_fabric_groups: list[str] = []
         self._mandatory_parameters_all_fabric_groups.append("FABRIC_NAME")
         self._mandatory_parameters_all_fabric_groups.append("FABRIC_TYPE")
 
@@ -93,93 +124,131 @@ class FabricGroupTypes:
         self._mandatory_parameters["MCFG"] = copy.copy(self._mandatory_parameters_all_fabric_groups)
         self._mandatory_parameters["MCFG"].sort()
 
-    def _init_properties(self) -> None:
-        """
-        Initialize properties specific to this class
-        """
-        self._template_name: str = ""
-        self._fabric_group_type: str = ""
-
     @property
-    def fabric_group_type(self):
+    def fabric_group_type(self) -> str:
         """
+        # Summary
+
         -   getter: Return the currently-set fabric group type.
         -   setter: Set the fabric group type.
-        -   setter: raise ``ValueError`` if value is not a valid fabric group type
+
+        ## Raises
+
+        -   setter: `ValueError` if value is not a valid fabric group type
         """
         return self._fabric_group_type
 
     @fabric_group_type.setter
-    def fabric_group_type(self, value):
-        """
-        -   Set the fabric group type.
-        -   raise ``ValueError`` if value is not a valid fabric group type
-        """
+    def fabric_group_type(self, value: str) -> None:
+        method_name: str = inspect.stack()[0][3]
         if value not in self._valid_fabric_group_types:
-            msg = f"{self.class_name}.fabric_group_type.setter: "
+            msg = f"{self.class_name}.{method_name}.setter: "
             msg += f"Invalid fabric group type: {value}. "
-            msg += f"Expected one of: {', '.join(self._valid_fabric_group_types)}."
+            msg += "Expected one of: "
+            msg += f"{', '.join(self._valid_fabric_group_types)}."
             raise ValueError(msg)
         self._fabric_group_type = value
 
     @property
-    def feature_name(self):
+    def fabric_type(self) -> str:
         """
-        -   getter: Return the feature name that must be enabled on the controller
-            for the currently-set fabric group type.
-        -   getter: raise ``ValueError`` if FabricGroupTypes().fabric_group_type is not set.
+        # Summary
+
+        Return the internal FABRIC_TYPE name for the currently-set fabric group type.
+
+        ## Raises
+
+        -   `ValueError` if `fabric_group_type` is not set.
         """
+        method_name = inspect.stack()[0][3]
         if self.fabric_group_type is None:
-            msg = f"{self.class_name}.feature_name: "
+            msg = f"{self.class_name}.{method_name}: "
             msg += f"Set {self.class_name}.fabric_group_type before accessing "
-            msg += f"{self.class_name}.feature_name"
+            msg += f"{self.class_name}.{method_name}"
+            raise ValueError(msg)
+        return self._fabric_group_type_to_fabric_type_map[self.fabric_group_type]
+
+    @property
+    def feature_name(self) -> str:
+        """
+        # Summary
+
+        Return the feature name that must be enabled on the controller
+        for the currently-set fabric group type.
+
+        ## Raises
+
+        -   `ValueError` if `fabric_group_type` is not set.
+        """
+        method_name = inspect.stack()[0][3]
+        if self.fabric_group_type is None:
+            msg = f"{self.class_name}.{method_name}: "
+            msg += f"Set {self.class_name}.fabric_group_type before accessing "
+            msg += f"{self.class_name}.{method_name}"
             raise ValueError(msg)
         return self._fabric_group_type_to_feature_name_map[self.fabric_group_type]
 
     @property
-    def mandatory_parameters(self):
+    def mandatory_parameters(self) -> list[str]:
         """
-        -   getter: Return the mandatory playbook parameters for the
-            currently-set fabric group type as a sorted list().
-        -   getter: raise ``ValueError`` if FabricGroupTypes().fabric_group_type
-            is not set.
+        # Summary
+
+        Return the mandatory playbook parameters for the currently-set fabric group type as a sorted list().
+
+        ## Raises
+
+        -   `ValueError` if `fabric_group_type` is not set.
         """
+        method_name = inspect.stack()[0][3]
         if self.fabric_group_type is None:
-            msg = f"{self.class_name}.mandatory_parameters: "
+            msg = f"{self.class_name}.{method_name}: "
             msg += f"Set {self.class_name}.fabric_group_type before accessing "
-            msg += f"{self.class_name}.mandatory_parameters"
+            msg += f"{self.class_name}.{method_name}"
             raise ValueError(msg)
         return self._mandatory_parameters[self.fabric_group_type]
 
     @property
-    def template_name(self):
+    def template_name(self) -> str:
         """
-        -   getter: Return the template name for the currently-set fabric group type.
-        -   getter: raise ``ValueError`` if FabricGroupTypes().fabric_group_type is not set.
+        # Summary
+
+        Return the template name for the currently-set fabric group type.
+
+        ## Raises
+
+        -   `ValueError` if
+            -   `fabric_group_type` is not set.
         """
+        method_name = inspect.stack()[0][3]
         if self.fabric_group_type is None:
-            msg = f"{self.class_name}.template_name: "
+            msg = f"{self.class_name}.{method_name}: "
             msg += f"Set {self.class_name}.fabric_group_type before accessing "
-            msg += f"{self.class_name}.template_name"
+            msg += f"{self.class_name}.{method_name}"
             raise ValueError(msg)
-        try:
-            return self._fabric_group_type_to_template_name_map[self.fabric_group_type]
-        except KeyError:
-            msg = f"{self.class_name}.template_name: "
-            msg += f"Unknown fabric group type: {self.fabric_group_type}. "
-            msg += f"Expected one of: {', '.join(self._valid_fabric_group_types)}."
-            raise ValueError(msg) from None
+        return self._fabric_group_type_to_template_name_map[self.fabric_group_type]
 
     @property
-    def valid_fabric_group_types(self):
+    def valid_fabric_group_types(self) -> list[str]:
         """
+        # Summary
+
         Return a sorted list() of valid fabric group types.
+
+        ## Raises
+
+        None
         """
         return sorted(self._valid_fabric_group_types)
 
     @property
-    def valid_fabric_group_template_names(self):
+    def valid_fabric_group_template_names(self) -> list[str]:
         """
+        # Summary
+
         Return a sorted list() of valid fabric group template names.
+
+        ## Raises
+
+        None
         """
         return sorted(self._fabric_group_type_to_template_name_map.values())
