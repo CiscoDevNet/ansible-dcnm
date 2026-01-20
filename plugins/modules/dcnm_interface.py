4e53d648ca2b78d1d67e2b725e09622c14caed67
mwiebe <mwiebe@cisco.com>
2026-01-20 10:53:50 -0500

    Fix default int for state override

--- plugins/modules/dcnm_interface.py
+++ plugins/modules/dcnm_interface.py
@@ -4702,8 +4702,12 @@ class DcnmIntf:
         for item in self.pb_input:
             # For overridden state, we will not touch anything that is present in incoming config,
             # because those interfaces will anyway be modified in the current run
+            # Must check both interface name AND serial number to ensure we're comparing the same
+            # interface on the same switch (not just the same interface name on different switches)
             if (self.module.params["state"] == "overridden") and (
                 item["ifname"] == have["ifName"]
+            ) and (
+                item.get("sno") == have.get("serialNo")
             ):
                 return False, item["ifname"]
             if item.get("members"):
@@ -4885,29 +4889,48 @@ class DcnmIntf:
                     continue
 
                 if uelem is not None:
-                    # Before defaulting ethernet interfaces, check if they are
-                    # member of any port-channel. If so, do not default that
-                    rc, intf = self.dcnm_intf_can_be_replaced(have)
-                    if rc is True:
-                        self.dcnm_intf_merge_intf_info(
-                            uelem, self.diff_replace
+                    # Before defaulting ethernet interfaces, check if this interface is present in want.
+                    # If yes, ignore the interface, because configuration from want will be applied anyway.
+                    # This ensures that Ethernet interfaces removed from the playbook config are reset to default,
+                    # while those still in the config are left alone to be configured later.
+                    match_want = [
+                        d
+                        for d in self.want
+                        if (
+                            (
+                                name.lower()
+                                == d["interfaces"][0]["ifName"].lower()
+                            )
+                            and (str(sno) == str(d["interfaces"][0]["serialNumber"]))
+                            and (fabric == d["interfaces"][0]["fabricName"])
                         )
-                        self.changed_dict[0]["replaced"].append(
-                            copy.deepcopy(uelem)
-                        )
-                        delem["serialNumber"] = sno
-                        delem["ifName"] = name
-                        delem["fabricName"] = self.fabric
-                        if str(deploy).lower() == "true":
-                            # Do not create interface E1/x/y, interface is created with breakout
-                            pattern = r'^Ethernet1/\d+/\d+$'
-                            if_name = delem.get('ifName')
+                    ]
 
-                            if not re.match(pattern, if_name):
-                                self.diff_deploy.append(delem)
-                                self.changed_dict[0]["deploy"].append(
-                                    copy.deepcopy(delem)
-                                )
+                    # Only default/reset this interface if it's NOT in the playbook config
+                    if not match_want:
+                        # Before defaulting ethernet interfaces, check if they are
+                        # member of any port-channel. If so, do not default that
+                        rc, intf = self.dcnm_intf_can_be_replaced(have)
+                        if rc is True:
+                            self.dcnm_intf_merge_intf_info(
+                                uelem, self.diff_replace
+                            )
+                            self.changed_dict[0]["replaced"].append(
+                                copy.deepcopy(uelem)
+                            )
+                            delem["serialNumber"] = sno
+                            delem["ifName"] = name
+                            delem["fabricName"] = self.fabric
+                            if str(deploy).lower() == "true":
+                                # Do not create interface E1/x/y, interface is created with breakout
+                                pattern = r'^Ethernet1/\d+/\d+$'
+                                if_name = delem.get('ifName')
+
+                                if not re.match(pattern, if_name):
+                                    self.diff_deploy.append(delem)
+                                    self.changed_dict[0]["deploy"].append(
+                                        copy.deepcopy(delem)
+                                    )
             # Sub-interafces are returned as INTERFACE_ETHERNET in have_all. So do an
             # additional check to see if it is physical. If not assume it to be sub-interface
             # for now. We will have to re-visit this check if there are additional non-physical
